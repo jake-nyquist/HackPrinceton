4728b54f7a446f70f8204501298de0af32d251b8
Jake Nyquist <jakenyquist@gmail.com>
2015-11-13 23:11:03 -0600

    Initial commit

---
+++ JessicaHackathon.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/addevent')
+def add_event():
+    return 'This will eventually add an event'
+
+@app.route('/eventdetails/<int:eventID>')
+def event_detail(eventID):
+    return 'findEvent: ' + str(eventID)
+
+@app.route('/upvote/<int:eventID>')
+def upvote(eventID):
+    return 'upvote: ' + str(eventID)
+
+@app.route('/upvote/<int:eventID>')
+def upvote(eventID):
+    return 'upvote: ' + str(eventID)
+
+@app.route('/report/<int:eventID>')
+def report(eventID):
+    return 'report: ' + str(eventID)
+
+if __name__ == '__main__':
+    app.run()
